01898448c1356b843a58d9a3142b0589468707a7
denis-rossati <denis.rossatiramos@gmail.com>
2022-11-08 21:46:31 -0300

    Stage #2 attempt 1: Responde PONG on request

--- app/main.js
+++ app/main.js
@@ -1,6 +1,10 @@
-const net = require("net");
+const net = require('net');
 
-
-const server = net.createServer((connection) => {});
+const server = net.createServer((connection) => {
+	connection.on('data', () => {
+		connection.write('+PONG\r\n');
+		connection.end();
+	})
+});
 
 server.listen(6379, "127.0.0.1");
