1192bdb6a76d993ab3761b73a12e246c0fbd3423
denis-rossati <denis.rossatiramos@gmail.com>
2022-11-12 16:04:52 -0300

    Refactor ping

--- app/main.js
+++ app/main.js
@@ -21,59 +21,55 @@ function parseBulkStrings(message, socket, commands) {
 	} else {
 		throw new Error('Bulk string doesn\'t start with length size indicator');
 	}
-
 	if (/^\d+\r\n/.test(message)) {
-		strSize = message.match(/^d+/)[0];
+		strSize = message.match(/^\d+/)[0];
+		message = parseLineBreak(message.replace(/^\d+/, ''));
 		// \r\nFOO\r\n -> FOO
-		let strContent = parseLineBreak(message).match(/^.+\r\n/)[0].slice(0, -2);
+		let strContent = parseLineBreak(message).match(/^\w+\r\n/)[0].slice(0, -2);
 
 		if (strContent.length !== parseInt(strSize)) {
 			throw new Error('Bulk string doesn\'t start with length size digit');
 		}
 
 		const command = Object.keys(commands).find((command) => strContent.toLowerCase().trim().startsWith(command));
-
 		if (command) {
-			commands[command](message, socket);
+			commands[command](message, socket, commands);
 		}
 
 		parsedMessage = `${strContent}\r\n`;
 	}
 
-	return parsedMessage;
+	return parseValue(parsedMessage, socket, message);
 }
 
-function parseArray(message, socket) {
+function parseArray(message, socket, commands) {
 	const arrSize = parseInt(message[1]);
 
-	message = parseLineBreak(message.replace(/^\*\d/, ''));
-
+	message = parseLineBreak(message.replace(/^\*\d+/, ''));
 	for (let index = 0; index < arrSize; index += 1) {
-		parseValue(message, socket);
+		parseValue(message, socket, commands);
 	}
 }
 
 function parseLineBreak(message) {
-	if (/^\\r\\n/.test(message)) {
-		throw new Error('Parsing line break failed')
+	if (/^\r\n/.test(message)) {
+		return message.replace(/^\r\n/, '');
 	}
 
-	return message.replace(/^\\r\\n/, '');
+	return message;
 }
 
 function handleEcho(message, socket) {
 
 }
 
-function parseValue(message, socket) {
-	const commands = {
-		'ping': (_message, socket) => {
-			socket.write('+PONG\r\n');
-			message = message.replace(/^ping/, '')
-		},
-		'echo': handleEcho(message, socket),
-	}
+function handlePing(message, socket) {
+	socket.write('+PONG\r\n');
+	message = parseLineBreak(message.replace(/^ping/, ''))
+	return message;
+}
 
+function parseValue(message, socket, commands) {
 	const operators = {
 		'+': parseString,
 		'-': parseErrors,
@@ -84,12 +80,16 @@ function parseValue(message, socket) {
 	}
 
 	const operator = message[0];
-	console.log(message)
 	return operators[operator](message, socket, commands);
 }
 
 function parseMessage(message, socket) {
-	return parseValue(message, socket);
+	const commands = {
+		'ping': handlePing,
+		'echo': handleEcho,
+	}
+
+	return parseValue(message, socket, commands);
 }
 
 
@@ -104,4 +104,4 @@ const server = net.createServer((socket) => {
 server.listen(6379, '127.0.0.1');
 
 // parseMessage('"*1\\r\\n$4\\r\\nping\\r\\n');
-// console.log(parseMessage('*2\\r\\n$4\\r\\nECHO\\r\\n$3\\r\\nhey\\r\\n'))
+// parseMessage('*1\r\n$4\r\nping\r\n');
