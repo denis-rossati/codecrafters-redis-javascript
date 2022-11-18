9741602d9ae9834fa2b47343472459f6a2975fc5
denis-rossati <denis.rossatiramos@gmail.com>
2022-11-17 21:46:31 -0300

    empty commit

--- app/main.js
+++ app/main.js
@@ -172,7 +172,7 @@ function handleGet(message, socket) {
 	if (value) {
 		socket.write(`+${value}\r\n`)
 	} else {
-		socket.write(`*1\r\n$3\r\nnil\r\n`);
+		socket.write(`+\r\n$3\r\nnil\r\n`);
 	}
 
 	message = parseLineBreak(message.replace(/^\w+/, ''));
